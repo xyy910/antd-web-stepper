be62ce3b8112a4cbfb7352156c96a42521869a48
yang.zheng <yang.zheng@corp.elong.com>
2018-03-12 19:35:16 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Hh from 'oprate';
+
+let a = (
+    <Hh/>
+);
+
+let div = document.getElementById('root');
+ReactDom.render(a, div);
